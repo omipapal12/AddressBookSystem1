d803d068636f3fab5da320b3b177e6c78a28f54c
omipapal12 <omipapal12@gmail.com>
2021-07-27 13:37:07 +0530

    Added: Main Contact Details

---
+++ src/com/company/ContactDetails.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ContactDetails {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book System");
+        Contact person = new Contact();
+        AddressBook obj= new AddressBook();
+        obj.getMenu();
+    }
+}
