8a917e6e90f5518ba66ab1d678af7cde86b309b4
omipapal12 <omipapal12@gmail.com>
2021-08-12 12:52:16 +0530

    Added: Number Of Contact Person By City or State

--- src/addressbookmain/AddressBook.java
+++ src/addressbookmain/AddressBook.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class AddressBook {
-    public ArrayList<Contact> contactList = new ArrayList<>();
+    public static final ArrayList<Contact> contactList = new ArrayList<>();
     public static Map<String, Contact> nameHashMap = new HashMap<String, Contact>();
     public static Map<String, Contact> cityHashMap = new HashMap<String, Contact>();
     public static Map<String, Contact> stateHashMap = new HashMap<String, Contact>();
@@ -229,6 +229,33 @@ public class AddressBook {
                 return;
             default:
                 System.out.println("INVALID CHOICE!");
+        }
+    }
+
+    // method to count element by option
+    public static void countByOption() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Count City ");
+        System.out.println("2. Count State");
+        System.out.println("3. Back ");
+        System.out.println("Enter Your Choice : ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice) {
+            case 1:
+                Map<String, Long> countCity = contactList.stream()
+                        .collect(Collectors.groupingBy(e -> e.getCity(), Collectors.counting()));
+                System.out.println(countCity + "\n");
+                break;
+            case 2:
+                Map<String, Long> countState = contactList.stream()
+                        .collect(Collectors.groupingBy(e -> e.getState(), Collectors.counting()));
+                System.out.println(countState + "\n");
+                break;
+            case 3:
+                return;
+            default:
+                System.out.println("Invalid Option");
         }
     }
 }
